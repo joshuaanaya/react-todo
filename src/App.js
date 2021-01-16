426d58fe5143e5ef341320ac7162e983fca94cd6
joshuaanaya <j.anaya@outlook.com>
2021-01-16 00:50:54 -0800

    Form complete. Work on State change

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import ToDoList from './components/ToDoList';
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos ] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Josh's Todo List</h1>
       </header>
-      <Form />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
       <ToDoList />
     </div>
     
